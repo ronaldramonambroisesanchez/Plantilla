2ad61a1dc318c74762dd7355b3d19b7c8cd85ccf
ronaldramonambroisesanchez <67200559+ronaldramonambroisesanchez@users.noreply.github.com>
2020-07-10 19:06:41 -0400

    Add files via upload

---
+++ credito.php
@@ -0,0 +1,9 @@
+<?php require_once "vistas/parte_superior_credito.php"?>
+
+<!--INICIO DEL CONT PRINCIPAL-->
+<div class="container">
+<h1><strong>Seccion Creditos</strong></h1>
+</div>
+<!--FIN-->
+
+<?php require_once "vistas/parte_inferior_credito.php"?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php require_once "vistas/parte_superior.php"?>
+
+<!--INICIO DEL CONT PRINCIPAL-->
+<div class="container">
+<h1><strong>Contenido Principal</strong></h1>
+</div>
+<!--FIN-->
+
+<?php require_once "vistas/parte_inferior.php"?>
